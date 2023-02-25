4c9991d5e24b46c866f14d922182521cd8e969f8
Kazumi Inada <timer.ground.gm@gmail.com>
2023-02-25 18:59:24 +0900

    chore: Add ofxPoco as dependency

---
+++ addon_config.mk
@@ -0,0 +1,8 @@
+meta:
+	ADDON_NAME = ofxExportImageSequence
+	ADDON_DESCRIPTION = Capture and export sequences of image
+	ADDON_AUTHOR = @satoruhiga
+	ADDON_URL = http://github.com/satoruhiga/ofxExportImageSequence
+
+common:
+	ADDON_DEPENDENCIES = ofxPoco
